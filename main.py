ee70b4927e8f2440d030446f08736fcc5a8c3802
dennis <tbvjwjdgjss@gmail.com>
2023-05-25 18:25:42 +0900

    메인화면 객체화 + 검색어로 그래프 불러오기
    + 임시 버튼 생성

--- main.py
+++ main.py
@@ -1,135 +1,150 @@
 from tkinter import *
+from tkinter import font
 import xml.etree.ElementTree as ET
 import requests
 
 # 공공데이터 API 키
 api_key = ""
-
 url = "http://apis.data.go.kr/B551011/KorService1/"
 api_keyword = "searchKeyword1"
 api_location = "locationBasedList1"
 api_detail = "detailCommon1"
 api_image = "detailImage1"
 
-keyword = "강원"
+class MainGUI:
+    def __init__(self):
+        self.content_List = ["관광지", "문화시설", "레포츠", "숙박", "쇼핑", "음식점"]
+        self.content_colors = ["pink", "cyan", "antique white", "cornflower blue", "medium spring green", "purple"]
+        self.fileName = ["tour.png", "culture.png", "leports.png", "hotel.png", "shopping.png", "restaurant.png"]
+        self.buttonIamges = []
+        self.InitMain()
 
-keyword_params = {
-    "serviceKey": api_key,
-    "numOfRows": 2000,  # 최대 2000개의 관광 정보 데이터 요청
-    "MobileOS": "ETC",  # 필수 입력 데이터(사용x)
-    "MobileApp": "AppTest",  # 필수 입력 데이터(사용x)
-    "Keyword": keyword,  # 필수 입력 데이터(input 값을 넣음)
-}
+    def SearchPage(self, case):
+        print(self.content_List[case])
 
-response = requests.get(url + api_keyword, params=keyword_params)
-root = ET.fromstring(response.content)
-items = root.findall(".//item")
+    def InitMain(self):
+        self.root = Tk()
+        self.root.geometry("600x800")
+        self.root.title("관광지 정보")
 
-tourLists = []
-for item in items:
-    tour = {
-        "name": item.findtext("title"),  # 관광지 이름
-        "address": item.findtext("addr1"),  # 주소
-        "mapx": item.findtext("mapx"),  # 경도
-        "mapy": item.findtext("mapy"),  # 위도
-        "typeid": item.findtext("contenttypeid"),  # 관광 시설 구분 코드
-        # 12:관광지, 14:문화시설, 15:축제공연행사, 25:여행코스
-        # 28:레포츠, 32:숙박, 38:쇼핑, 39:음식점
-        "contentid": item.findtext("contentid"),  # 시설 고유 코드
-        "imageUrl": item.findtext("firstimage")  # 이미지 url
-    }
-    tourLists.append(tour)
+        # 폰트
+        self.main_font = font.Font(size=16, family='Consolas')
+        self.main_fontB = font.Font(size=12, weight='bold', family='Consolas')
 
-content_count = {
-    "관광지": 0,
-    "문화시설": 0,
-    "레포츠": 0,
-    "숙박": 0,
-    "쇼핑": 0,
-    "음식점": 0
-}
+        # frame1 : 검색 entry
+        self.frame1 = Frame(self.root, width=100, height=20)
+        self.frame1.place(x=100, y=0)
 
-content_colors = ["pink", "cyan", "antique white", "cornflower blue", "medium spring green", "purple"]
+        self.label_search = Label(self.frame1, text="검색", font=self.main_fontB)
+        self.entry_search = Entry(self.frame1, width=30, font=self.main_fontB)
+        self.label_search.pack(side=LEFT)
+        self.entry_search.pack()
 
-print("<<<<관광지 목록>>>>")
-for tour in tourLists:
-    if tour["typeid"] == '12':
-        content_count["관광지"] += 1
-        print(tour)
+        # frame2 : 그래프 그리는 캔버스
+        self.frame2 = Frame(self.root, width=500, height=500)
+        self.frame2.place(x=50, y=50)
 
-print()
-print()
+        self.canvas = Canvas(self.frame2, width=500, height=500, bg='white')
+        self.canvas.pack()
 
-print("<<<<문화시설 목록>>>>")
-for tour in tourLists:
-    if tour["typeid"] == '14':
-        content_count["문화시설"] += 1
-        print(tour)
+        # frame3 : 버튼 5개
+        self.frame3 = Frame(self.root, width=600, height=200)
+        self.frame3.place(x=20, y=580)
 
-print()
-print()
-
-print("<<<<레포츠 목록>>>>")
-for tour in tourLists:
-    if tour["typeid"] == '28':
-        content_count["레포츠"] += 1
-        print(tour)
-
-print()
-print()
-
-print("<<<<숙박 목록>>>>")
-for tour in tourLists:
-    if tour["typeid"] == '32':
-        content_count["숙박"] += 1
-        print(tour)
-
-print()
-print()
-
-print("<<<<쇼핑 목록>>>>")
-for tour in tourLists:
-    if tour["typeid"] == '38':
-        content_count["쇼핑"] += 1
-        print(tour)
-
-print()
-print()
-
-print("<<<<음식점 목록>>>>")
-for tour in tourLists:
-    if tour["typeid"] == '39':
-        content_count["음식점"] += 1
-        print(tour)
-
-root = Tk()
-root.title("서울시 구별 병원 정보")
-
-# 캔버스 생성
-canvas = Canvas(root, width=800, height=400)
-canvas.pack()
-
-max_content_count = max(content_count.values())
-bar_width = 20
-x_gap = 30
-y_gap = 30
-# x0 = 60
-# y0 = 250
-x0 = 60
-y0 = 30
-content_names = list(content_count.keys())
-content_values = list(content_count.values())
+        # 버튼
+        self.buttons = []
+        for i in range(6):
+            self.buttons.append(
+                Button(self.frame3, text=self.content_List[i], width=6, height=4, font= self.main_font,
+                       command=lambda case=i: self.SearchPage(case)).place(x=i * 95, y=10))
 
 
-for i in range(6):
-    x1 = x0 + 200 * content_values[i] / max_content_count
-    y1 = y0 + i * (bar_width + y_gap)
-    # x1 = x0 + i * (bar_width + x_gap)
-    # y1 = y0 - 200 * content_values[i] / max_content_count
-    canvas.create_rectangle(x0, y1, x1, y1 + bar_width, fill=content_colors[i])
-    canvas.create_text(x0 - 30, y1 + bar_width / 2, text=content_names[i], anchor='s')
-    canvas.create_text(x1 + 10, y1 + bar_width / 2, text=content_values[i], anchor='s')
-    # canvas.create_text(x1 + bar_width / 2, y0 + 100, text=content_names[i], anchor='s')
-    # canvas.create_text(x1 + bar_width / 2, y1 - 10, text=content_values[i], anchor='s')
+        self.GetXML("")
 
-root.mainloop()
+        self.entry_search.bind("<Return>", self.GetXML)
+
+        self.root.mainloop()
+
+    def GetXML(self, str):
+        self.keyword = self.entry_search.get()
+
+        self.keyword_params = {
+            "serviceKey": api_key,
+            "numOfRows": 20000,  # 최대 2000개의 관광 정보 데이터 요청
+            "MobileOS": "ETC",  # 필수 입력 데이터(사용x)
+            "MobileApp": "AppTest",  # 필수 입력 데이터(사용x)
+            "Keyword": self.keyword,  # 필수 입력 데이터(input 값을 넣음)
+        }
+
+        response = requests.get(url + api_keyword, params=self.keyword_params)
+        root = ET.fromstring(response.content)
+        items = root.findall(".//item")
+
+        self.tourLists = []
+
+        for item in items:
+            self.tour = {
+                "name": item.findtext("title"),  # 관광지 이름
+                "address": item.findtext("addr1"),  # 주소
+                "mapx": item.findtext("mapx"),  # 경도
+                "mapy": item.findtext("mapy"),  # 위도
+                "typeid": item.findtext("contenttypeid"),  # 관광 시설 구분 코드
+                # 12:관광지, 14:문화시설, 15:축제공연행사, 25:여행코스
+                # 28:레포츠, 32:숙박, 38:쇼핑, 39:음식점
+                "contentid": item.findtext("contentid"),  # 시설 고유 코드
+                "imageUrl": item.findtext("firstimage")  # 이미지 url
+            }
+            self.tourLists.append(self.tour)
+
+        self.content_count = {
+            "관광지": 0,
+            "문화시설": 0,
+            "레포츠": 0,
+            "숙박": 0,
+            "쇼핑": 0,
+            "음식점": 0
+        }
+
+        for self.tour in self.tourLists:
+            if self.tour["typeid"] == '12':
+                self.content_count["관광지"] += 1
+            elif self.tour["typeid"] == '14':
+                self.content_count["문화시설"] += 1
+            elif self.tour["typeid"] == '28':
+                self.content_count["레포츠"] += 1
+            elif self.tour["typeid"] == '32':
+                self.content_count["숙박"] += 1
+            elif self.tour["typeid"] == '38':
+                self.content_count["쇼핑"] += 1
+            elif self.tour["typeid"] == '39':
+                self.content_count["음식점"] += 1
+
+        self.DrawGraph()
+
+
+    def DrawGraph(self):
+        self.canvas.destroy()
+        self.canvas = Canvas(self.frame2, width=500, height=500, bg='white')
+        self.canvas.pack()
+        # 그래프 막대 그리기
+        max_content_count = max(self.content_count.values())
+        bar_width = 30
+        y_gap = 50
+        x0 = 80
+        y0 = 30
+        content_names = list(self.content_count.keys())
+        content_values = list(self.content_count.values())
+
+        for i in range(6):
+            x1 = x0 + 400 * content_values[i] / max(max_content_count, 1)
+            y1 = y0 + i * (bar_width + y_gap)
+            self.canvas.create_rectangle(x0, y1, x1, y1 + bar_width, fill=self.content_colors[i])
+            self.canvas.create_text(x0 - 40, y1 + bar_width / 2, text=content_names[i], anchor='s')
+            self.canvas.create_text(x1 + 10, y1 + bar_width / 2, text=content_values[i], anchor='s')
+
+
+MainGUI()
+
+
+
+
